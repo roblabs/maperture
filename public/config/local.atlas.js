a1a8a8693521708589da757e1cf676066e7837c4
RobLabs.com <roblabs@users.noreply.github.com>
2023-01-04 11:00:49 -0800

    Publish public domain maps for reviewing issues.  These are already published on branch `gh-pages`

---
+++ public/config/local.atlas.js
@@ -0,0 +1,55 @@
+const mapboxGlAccessToken =
+  'pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg';
+
+// Set `mapbox.baseApiUrl` if your Mapbox flavored style uses a different server than `api.mapbox.com`.
+const mapboxBaseApiUrl = 'https://atlas-stg.geoplatform.gov';
+
+// Example of setting up a gazetteer object. This is described in the `README`
+// but these are the options that populate the dropdown in the UI that take you
+// to specific locations or map views. You can add as many as you like here and
+// create groupings (in this example, the only group is called Locations).
+const gazetteer = {
+  Units: [
+    {
+      'San Francisco, CA': {
+        zoom: 6,
+        center: { lng: -122.4193, lat: 37.7648 },
+      },
+    },
+    {
+      'Washington DC': {
+        zoom: 6,
+        center: { lng: -77.0435, lat: 38.9098 },
+      },
+    },
+  ],
+};
+
+const stylePresets = [
+  {
+    id: 'ck58pyquo009v01p99xebegr9',
+    name: 'Park Tiles 4 Standard',
+    type: 'mapbox-gl',
+    url: 'https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck58pyquo009v01p99xebegr9?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg',
+  },
+  {
+    id: 'ck72fwp2642dv07o7tbqinvz4',
+    name: 'Park Tiles 4 Imagery',
+    type: 'mapbox-gl',
+    url: 'https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck72fwp2642dv07o7tbqinvz4?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg',
+  },
+  {
+    id: 'ck5cpia2u0auf01p9vbugvcpv',
+    name: 'Park Tiles 4 Light',
+    type: 'mapbox-gl',
+    url: 'https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpia2u0auf01p9vbugvcpv?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg',
+  },
+  {
+    id: 'ckevxkdh1k0ru07r0k2eznkzp',
+    name: 'Park Tiles 4 Slate',
+    type: 'mapbox-gl',
+    url: 'https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ckevxkdh1k0ru07r0k2eznkzp?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg',
+  },
+];
+
+export { gazetteer, mapboxGlAccessToken, mapboxBaseApiUrl, stylePresets };
---
+++ public/config/local.open-source.js
@@ -0,0 +1,45 @@
+// const mapboxGlAccessToken = '';  /* Token is Not used in the MapLibre demos */
+
+// Example of setting up a gazetteer object. This is described in the `README`
+// but these are the options that populate the dropdown in the UI that take you
+// to specific locations or map views. You can add as many as you like here and
+// create groupings (in this example, the only group is called Locations).
+const gazetteer = {
+  Locations: [
+    {
+      'San Francisco, CA': {
+        zoom: 6,
+        center: { lng: -122.4193, lat: 37.7648 },
+      },
+    },
+    {
+      'Washington DC': {
+        zoom: 6,
+        center: { lng: -77.0435, lat: 38.9098 },
+      },
+    },
+  ],
+};
+
+const stylePresets = [
+  {
+    id: 'maplibre-demotiles',
+    name: 'MapLibre',
+    type: 'maplibre-gl',
+    url: 'https://demotiles.maplibre.org/style.json',
+  },
+  {
+    id: 'naturalearthtiles',
+    name: 'Natural Earth Tiles',
+    type: 'maplibre-gl',
+    url: 'https://raw.githubusercontent.com/roblabs/xyz-raster-sources/master/styles/naturalearthtiles.json',
+  },
+  {
+    id: 'geography-class',
+    name: 'Geography Class',
+    type: 'maplibre-gl',
+    url: 'https://raw.githubusercontent.com/roblabs/openmaptiles-ios-demo/master/OSM2VectorTiles/styles/geography-class.GitHub.json',
+  },
+];
+
+export { gazetteer, stylePresets };
